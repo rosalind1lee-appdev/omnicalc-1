e970aa23b422977e7ebafc28965cfd4ed587b8e2
rosalind1lee <rlee90@chicagobooth.edu>
2021-04-18 01:50:15 +0000

    payment works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 	  render({ :template => "calculation_templates/squareroot_form.html.erb" })
   end
 
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
   def blank_random_form
     render({ :template => "calculation_templates/rand_form.html.erb" })
   end
@@ -15,6 +19,23 @@ class ApplicationController < ActionController::Base
 	  @num = params.fetch("elephant").to_f
 	  @square_of_num = @num ** 2
 	  render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+  def calculate_payment
+    @apr = params.fetch("apr_input").to_f
+    @years = params.fetch("years_input").to_i
+    @prin = params.fetch("principal_input").to_f
+    
+    monthlyrate = @apr/(12.0 * 100)
+    num_months = @years * 12
+
+    numer = @prin*monthlyrate*(1+monthlyrate)**num_months
+    denom = (1+monthlyrate)**num_months-1
+    @result = (numer*1.0/denom).round(2)
+    @round_apr = "#{@apr.round(4)}%"
+
+    
+	  render({ :template => "calculation_templates/payment_results.html.erb" })
   end
 
   def calculate_squareroot
