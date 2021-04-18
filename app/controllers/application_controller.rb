f367c2e8cedb9d6c24a517d4948a3ba9909457e9
rosalind1lee <rlee90@chicagobooth.edu>
2021-04-18 00:43:03 +0000

    square works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+	  render({ :template => "calculation_templates/square_form.html.erb" })
+  end
 
+  def calculate_square
+	  @num = params.fetch("elephant").to_f
+	  @square_of_num = @num ** 2
+	  render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+  def calculate_random
+	  @lower = params.fetch("user_min").to_f
+	  @upper = params.fetch("user_max").to_f
+	  @result = rand(@lower..@upper)
+	  render({ :template => "calculation_templates/rand_results.html.erb" })
+  end
 end
